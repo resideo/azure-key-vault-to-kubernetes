e29680ac1ca1d2fe5441c04aa0b1783203efa574
Abhilash Joseph <abhilash.joseph@honeywell.com>
2020-02-10 10:51:36 -0500

    CERES-476 fix permission set order

--- cmd/azure-keyvault-secrets-webhook/main.go
+++ cmd/azure-keyvault-secrets-webhook/main.go
@@ -87,12 +87,11 @@ func setLogLevel(logLevel string) {
 func getInitContainers() []corev1.Container {
 	cmd := "cp /usr/local/bin/azure-keyvault-env /azure-keyvault/"
 
-	cmd = cmd + fmt.Sprintf(" && chmod -R 777 %s", "/azure-keyvault")
-
 	if !config.customAuth {
-		cmd = cmd + fmt.Sprintf(" && cp %s %s && ", config.cloudConfigHostPath, config.cloudConfigContainerPath)
-		cmd = cmd + fmt.Sprintf("chmod 444 %s", config.cloudConfigContainerPath)
+		cmd = cmd + fmt.Sprintf(" && cp %s %s ", config.cloudConfigHostPath, config.cloudConfigContainerPath)
 	}
+
+	cmd = cmd + fmt.Sprintf(" && chmod -R a+w %s", "/azure-keyvault")
 
 	container := corev1.Container{
 		Name:            "copy-azurekeyvault-env",
